0d1492ba05ea1a753bfa30b557b15fa759be23eb
Timur Agafonov <electroidios@gmail.com>
2024-05-31 23:20:06 +0300

    fix debug_log_true.py again

--- test_library/debug_log_true.py
+++ test_library/debug_log_true.py
@@ -8,9 +8,9 @@ class DebugLogs():
     def debug_logs(file_name, response_json, status_code, url, url_2, url_3=None, url_currency=None,
                    url_clientSecret=None,request_headers="", request_body=""):
         url_base = "https://p2psys-publicoffice.konomik.com"
-        print("\033[92m" + "\n" + file_name + "\033[94m")
+        print("\033[92m" + "\n" + file_name + "\n" + "\033[94m")
         print("----------------------------------------Request---------------------------------------")
-        print("\033[93m" + url_base + url_2)
+        print("\033[93m" + "\n" + url_base + url_2)
         if url_3 is not None: print(url_3)
         if url_currency is not None: print(url_currency)
         if url_clientSecret is not None: print(url_clientSecret)
@@ -19,8 +19,10 @@ class DebugLogs():
         print("\033[94m")
         print("----------------------------------------Response---------------------------------------")
         print("\033[93m" + "\n" + json.dumps(response_json, indent=2))
-        print("\033[92m" + f"response status code: {status_code}")
+        print("\033[92m" + "\n" + f"response status code: {status_code}")
         print(url)
-
+        print("\033[94m")
+        print("----------------------------------------Assertions-------------------------------------")
+        print("\033[92m")
 #  url_3, url_currency, url_clientSecret, request_headers,
 #  url_3 + url_currency + url_clientSecret + request_headers +
