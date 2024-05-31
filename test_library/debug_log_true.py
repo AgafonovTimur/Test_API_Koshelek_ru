2bac4bfe00c9dd58e7b1119334242298877ff55a
Timur Agafonov <electroidios@gmail.com>
2024-05-31 22:59:44 +0300

    add request to debug_log_true.py finished 2

--- test_library/debug_log_true.py
+++ test_library/debug_log_true.py
@@ -8,14 +8,17 @@ class DebugLogs():
     def debug_logs(file_name, response_json, status_code, url, url_2, url_3=None, url_currency=None,
                    url_clientSecret=None,request_headers="", request_body=""):
         url_base = "https://p2psys-publicoffice.konomik.com"
-        print("\033[92m" + "\n" + file_name + "\n" + "\033[94m" + "Request : ")
+        print("\033[92m" + "\n" + file_name + "\033[94m")
+        print("----------------------------------------Request---------------------------------------")
         print("\033[93m" + url_base + url_2)
         if url_3 is not None: print(url_3)
         if url_currency is not None: print(url_currency)
         if url_clientSecret is not None: print(url_clientSecret)
         if request_headers is not None: print(request_headers)
         if request_body is not None: print(request_body)
-        print("\033[94m" + "Response : " + "\033[93m" + "\n" + json.dumps(response_json, indent=2))
+        print("\033[94m")
+        print("----------------------------------------Response---------------------------------------")
+        print("\033[93m" + "\n" + json.dumps(response_json, indent=2))
         print("\033[92m" + f"response status code: {status_code}")
         print(url)
 
