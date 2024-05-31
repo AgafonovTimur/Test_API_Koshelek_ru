24ce9b050afe9fe168245eabbfb2b0c07dac3931
Timur Agafonov <electroidios@gmail.com>
2024-05-31 22:08:11 +0300

    add request params in debug_log_true.py

--- test_library/debug_log_true.py
+++ test_library/debug_log_true.py
@@ -1,15 +1,23 @@
-
 import json
 
 debug_true = True
 
+
 class DebugLogs():
     @staticmethod
-    def debug_logs(*args):
-        file_name, response_json, status_code, url = args
-
-        print("\033[92m" + "\n" + file_name + "\n" + "\033[93m"
-              + json.dumps(response_json, indent=2))
+    def debug_logs(file_name, response_json, status_code, url, url_2, url_3=None, url_currency=None,
+                   url_clientSecret=None,request_headers="", request_body=""):
+        url_base = "https://p2psys-publicoffice.konomik.com"
+        print("\033[92m" + "\n" + file_name + "\n" + "Request : ")
+        print("\033[93m" + url_base + url_2)
+        if url_3 is not None: print(url_3)
+        if url_currency is not None: print(url_currency)
+        if url_clientSecret is not None: print(url_clientSecret)
+        if request_headers is not None: print(request_headers)
+        if request_body is not None: print(request_body)
+        print("\033[92m" + "Response : " + "\033[93m" + "\n" + json.dumps(response_json, indent=2))
         print("\033[92m" + f"response status code: {status_code}")
         print(url)
 
+#  url_3, url_currency, url_clientSecret, request_headers,
+#  url_3 + url_currency + url_clientSecret + request_headers +
