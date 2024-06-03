8752b7e9a60fe813c3c197b29122452821bde4ec
Timur Agafonov <electroidios@gmail.com>
2024-06-03 22:03:06 +0300

    playwright change from chromium to chrome

--- Autotests_with_pytest/API_tests/0_autorization/test_01_correct_client_ID_and_secret.py
+++ Autotests_with_pytest/API_tests/0_autorization/test_01_correct_client_ID_and_secret.py
@@ -23,4 +23,4 @@ class Test:
         Assertions.status_code_check(response.status_code, 200)
         Assertions.json_result_success(response.json()["result"]["success"], True)
         Assertions.json_result_errorData(response.json()["result"]["errorData"], {})
-        # Branch_change_test
+
--- Autotests_with_pytest/UI_Tests/test1.py
+++ Autotests_with_pytest/UI_Tests/test1.py
@@ -1,8 +1,10 @@
+import os
 from playwright.sync_api import Playwright
 
 
 def test_run(playwright: Playwright) -> None:
-    browser = playwright.chromium.launch(headless=False)
+
+    browser = playwright.chromium.launch(channel='chrome', headless=False)
     context = browser.new_context()
     page = context.new_page()
 
@@ -14,6 +16,7 @@ def test_run(playwright: Playwright) -> None:
 
     page.goto("https://www.google.com/")
     assert page.url == "https://www.google.com/"
+    page.wait_for_timeout(100)
     page.get_by_label("Найти").click()
     page.get_by_label("Найти").fill("playwrite documentation")
     page.get_by_label("Найти").press("Enter")
